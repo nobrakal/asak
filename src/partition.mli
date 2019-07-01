d6c5caa82d2bbc9a16c560aaa13835f7fda94fda
Alexandre Moine <alexandre@moine.me>
2019-07-01 10:23:21 +0200

    Start the use of odoc for Partition

--- src/partition.mli
+++ src/partition.mli
@@ -5,14 +5,35 @@
  * asak is distributed under the terms of the MIT license. See the
  * included LICENSE file for details. *)
 
+(** Type used for presenting a partition. *)
 type 'a partition =
   { bad_type : 'a list ;
+    (** Contains keys identifying codes that either don't implement the searched function,
+        or implement it with a bad type. *)
     clusters : 'a list Wtree.wtree list;
+    (** Main result.
+        Each class is composed of a tree representing a hierarchical clustering of "similar codes".
+        Each leaf is a list of keys identifying "totally similar" codes.
+*)
   }
 
+(** Partition OCaml codes
+    @param int Weight threshold required to keep sub-AST. It is exprimed
+as percent (a number between 0 and 100) in term of the weight of the whole AST.
+
+- A value of 0 means "keep all sub-ASTs"
+- A value of 100 means "don't keep any sub-AST"
+
+    @param string1 The name of the searched function
+    @param string2 A reference implementation, used only for typing
+    @param list List of valid OCaml codes, using only the standard library, that may
+contains the searched function.
+
+         @return The partition
+*)
 val partition :
-  int (* Percent of subtrees to keep *)
-  -> string (* The searched function *)
-  -> string (* A reference implementation, used only for typing *)
-  -> ('a * string) list (* List of codes maybe containing the function, each identified by a key *)
+  int
+  -> string
+  -> string
+  -> ('a * string) list
   -> 'a partition
