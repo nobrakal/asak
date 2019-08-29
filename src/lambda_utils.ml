7177ea9568c51dc60c333361f9df5e684bce6894
Alexandre Moine <alexandre@moine.me>
2019-08-29 21:55:54 +0200

    facotrize

--- src/lambda_utils.ml
+++ src/lambda_utils.ml
@@ -174,21 +174,17 @@ let hash_lambda is_sorting threshold l =
   let fmain_weight = float_of_int main_weight in
   (main_weight,h), sort_filter is_sorting threshold fmain_weight ss_arbres
 
-(* Replace every occurence of ident by its body *)
-let replace ident body =
+let fold_lambda lvar llet =
   let rec aux expr =
     let insnd lst = List.map (fun (e,x) -> e, aux x) lst in
     let inopt = function
     | None -> None
     | Some x -> Some (aux x) in
     match expr with
-    | Lvar x ->
-       if x = ident
-       then body
-       else expr
+    | Lvar x -> lvar x
     | Lconst _ -> expr
     | Llet (k,e,ident,l,r) ->
-       Llet (k, e, ident, aux l, aux r)
+       llet aux k e ident l r
   | Lapply x ->
      let ap_func = aux x.ap_func in
      let ap_args = List.map aux x.ap_args in
@@ -237,6 +233,15 @@ let replace ident body =
      Lsend (a, aux b, aux c, List.map aux d, e)
   in aux
 
+(* Replace every occurence of ident by its body *)
+let replace ident body =
+  let lvar x =
+    if x = ident
+    then body
+    else Lvar x in
+  let llet aux a b c d e = Llet (a,b,c,aux d,aux e) in
+  fold_lambda lvar llet
+
 (* Is the definition inlineable ? *)
 let inlineable x f =
   match x with
@@ -251,62 +256,12 @@ let inlineable x f =
 
 (* Inline all possible "let definitions"
    (that is, all "let definitions" without a side effet) *)
-let rec inline_all x =
-  let insnd lst = List.map (fun (e,l) -> e,inline_all l) lst in
-  let inopt = function
-    | None -> None
-    | Some x -> Some (inline_all x) in
-  match x with
-  | Lvar _ | Lconst _ -> x
-  | Llet (k,e,ident,l,r) ->
-     if inlineable k l
-     then
-       inline_all (replace ident l r)
-     else
-       Llet (k, e, ident, inline_all l, inline_all r)
-  | Lapply x ->
-     let ap_func = inline_all x.ap_func in
-     let ap_args = List.map inline_all x.ap_args in
-     Lapply {x with ap_func; ap_args }
-  | Lfunction x ->
-     let body = inline_all x.body in
-     Lfunction {x with body}
-  | Lletrec (lst,l) ->
-     Lletrec (insnd lst, inline_all l)
-  | Lprim (a,lst,b) ->
-     Lprim (a,List.map inline_all lst, b)
-  | Lstaticraise (a,lst) ->
-     Lstaticraise (a,List.map inline_all lst)
-  | Lifthenelse (i,f,e) ->
-     Lifthenelse (inline_all i, inline_all f, inline_all e)
-  | Lsequence (l,r) ->
-     Lsequence (inline_all l, inline_all r)
-  | Lwhile (l,r) ->
-     Lwhile (inline_all l, inline_all r)
-  | Lifused (i,l) ->
-     Lifused (i, inline_all l)
-#if OCAML_VERSION >= (4, 06, 0)
-  | Lswitch (l,s,i) ->
-     let sw_consts = insnd s.sw_consts in
-     let sw_blocks = insnd s.sw_blocks in
-     Lswitch (inline_all l, {s with sw_consts; sw_blocks},i)
-#else
-  | Lswitch (l,s) ->
-     let sw_consts = insnd s.sw_consts in
-     let sw_blocks = insnd s.sw_blocks in
-     Lswitch (inline_all l, {s with sw_consts; sw_blocks})
-#endif
-  | Lstringswitch (l,lst,opt,e) ->
-     Lstringswitch (inline_all l, insnd lst, inopt opt, e)
-  | Lassign (i,l) ->
-     Lassign (i, inline_all l)
-  | Levent (l,e) ->
-     Levent (inline_all l, e)
-  | Lstaticcatch (l,lst,r) ->
-     Lstaticcatch (inline_all l, lst, inline_all r)
-  | Ltrywith (l,i,r) ->
-     Ltrywith (inline_all l, i, inline_all r)
-  | Lfor (e,a,b,d,c) ->
-     Lfor (e, inline_all a, inline_all b, d, inline_all c)
-  | Lsend (a,b,c,d,e) ->
-     Lsend (a, inline_all b, inline_all c, List.map inline_all d, e)
+let inline_all =
+  let lvar x = Lvar x in
+  let llet aux k e ident l r =
+    if inlineable k l
+    then
+      aux (replace ident l r)
+    else
+      Llet (k, e, ident, aux l, aux r) in
+  fold_lambda lvar llet
