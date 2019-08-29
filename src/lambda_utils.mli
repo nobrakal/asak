58e64681c37f2768e6d000e74aa4c38003d51ae6
Alexandre Moine <alexandre@moine.me>
2019-08-29 22:45:07 +0200

    More inline

--- src/lambda_utils.mli
+++ src/lambda_utils.mli
@@ -28,3 +28,6 @@ val hash_lambda :
 
 (** Inline all possible (ie. without side effect) expressions in a lambda expression. *)
 val inline_all : Lambda.lambda -> Lambda.lambda
+
+(** When two applications are composed, make only one. *)
+val apply_full : Lambda.lambda -> Lambda.lambda
