650d65e64fd3d748e9ecf51e4736e29330b38e08
Alexandre Moine <alexandre@moine.me>
2019-09-06 12:55:53 +0200

    Better hash for partial application and inlining

--- src/lambda_utils.ml
+++ src/lambda_utils.ml
@@ -10,14 +10,15 @@ open Asttypes
 
 type threshold = Percent of int | Hard of int
 
-let h1 x = 1,[],Digest.string x
+let h1' x = 1,[],x
+let h1  x = h1' (Digest.string x)
 
 let hash_string_lst is_sorting x xs =
   let sort xs =
     if is_sorting
     then List.sort compare xs
     else xs in
-  let p,lst, xs =
+  let p,lst,xs =
     List.fold_right
       (fun (u,l,x) (v,l',xs) -> u+v,(u,x)::l@l',x::xs)
       xs (0,[],[]) in
@@ -25,8 +26,14 @@ let hash_string_lst is_sorting x xs =
     Digest.string @@
       String.concat "" @@
         sort @@
-          Digest.string x::xs in
+          if x = "" then xs else (Digest.string x::xs) in
   1+p,lst,res
+
+let hash_incremental start xs =
+  let p,lst,xs' =
+    List.fold_right (fun (u,l,x) (v,l',xs) -> u+v,(u,x)::l@l',Digest.string (x^xs))
+      xs start in
+  1+p,lst,xs'
 
 let hash_lst is_sorting f x xs =
   let res =
@@ -61,52 +68,57 @@ let hash_lambda is_sorting =
   let hash_string_lst = hash_string_lst is_sorting in
   let hash_lst_anon f = hash_lst_anon is_sorting f in
   let hash_lst f = hash_lst is_sorting f in
-  let rec hash_lambda = function
-  | Lvar _ -> h1 "Lvar"
+  let rec hash_lambda letbinds x =
+    let hash_lambda' = hash_lambda letbinds in
+  match x with
+  | Lvar x ->
+       begin
+         match List.assoc_opt x letbinds with
+         | None -> h1 "Lvar"
+         | Some x' -> h1' x'
+       end
   | Lconst _ -> h1 "Lconst"
   | Lapply x ->
-     hash_string_lst "Lapply"
-       [ hash_lambda x.ap_func
-       ; hash_lst_anon hash_lambda x.ap_args
-       ]
+     hash_incremental (hash_lambda' x.ap_func) (List.map hash_lambda' (x.ap_args))
   | Lfunction x ->
      hash_string_lst "Lfunction"
-       [ hash_lambda x.body ]
-  | Llet (_,_,_,l,r) ->
+       [ hash_lambda' x.body ]
+  | Llet (_,_,id,l,r) ->
+     let ((_,_,h) as lefthash) = hash_lambda' l in
      hash_string_lst "Llet"
-       [ hash_lambda l
-       ; hash_lambda r]
+       [ lefthash
+       ; hash_lambda ((id,h)::letbinds) r]
   | Lletrec (lst,l) ->
      hash_string_lst "Lletrec"
-       [ hash_lst_anon (fun (_,x) -> hash_lambda x) lst
-       ; hash_lambda l]
+       [ hash_lst_anon (fun (_,x) -> hash_lambda' x) lst
+       ; hash_lambda' l]
   | Lprim (_,lst,_) ->
      hash_string_lst "Lprim"
-       [ hash_lst_anon hash_lambda lst
+       [ hash_lst_anon hash_lambda' lst
        ]
   | Lstaticraise (_,lst) ->
      hash_string_lst "Lstaticraise"
-       [ hash_lst_anon hash_lambda lst
+       [ hash_lst_anon hash_lambda' lst
        ]
   | Lifthenelse (i,f,e) ->
      hash_string_lst "Lifthenelse"
-       [ hash_lambda i
-       ; hash_lambda f
-       ; hash_lambda e
+       [ hash_lambda' i
+       ; hash_lambda' f
+       ; hash_lambda' e
        ]
   | Lsequence (l,r) ->
      hash_string_lst "Lsequence"
-       [ hash_lambda l
-       ; hash_lambda r
+       [ hash_lambda' l
+       ; hash_lambda' r
        ]
   | Lwhile (l,r) ->
      hash_string_lst "Lwhile"
-       [ hash_lambda l
-       ; hash_lambda r
+       [ hash_lambda' l
+       ; hash_lambda' r
        ]
   | Lifused (_,l) ->
      hash_string_lst "Lifused"
-       [ hash_lambda l
+       [ hash_lambda' l
        ]
 #if OCAML_VERSION >= (4, 06, 0)
   | Lswitch (l,s,_) ->
@@ -114,50 +126,50 @@ let hash_lambda is_sorting =
   | Lswitch (l,s) ->
 #endif
      hash_string_lst "Lswitch"
-       [ hash_lambda l
-       ; hash_lst (hash_case string_of_int hash_lambda) "sw_consts" s.sw_consts
-       ; hash_lst (hash_case string_of_int hash_lambda) "sw_blocks" s.sw_blocks
-       ; hash_option hash_lambda s.sw_failaction
+       [ hash_lambda' l
+       ; hash_lst (hash_case string_of_int hash_lambda') "sw_consts" s.sw_consts
+       ; hash_lst (hash_case string_of_int hash_lambda') "sw_blocks" s.sw_blocks
+       ; hash_option hash_lambda' s.sw_failaction
        ]
   | Lstringswitch (l,lst,opt,_) ->
      hash_string_lst "Lstringswitch"
-       [ hash_lambda l
-       ; hash_lst (hash_case (fun x -> x) hash_lambda) "sw_consts" lst
-       ; hash_option hash_lambda opt
+       [ hash_lambda' l
+       ; hash_lst (hash_case (fun x -> x) hash_lambda') "sw_consts" lst
+       ; hash_option hash_lambda' opt
        ]
   | Lassign (_,l) ->
      hash_string_lst "Lassign"
-       [ hash_lambda l
+       [ hash_lambda' l
        ]
   | Levent (l,_) ->
      hash_string_lst "Levent"
-       [ hash_lambda l
+       [ hash_lambda' l
        ]
   | Lstaticcatch (l,_,r) ->
      hash_string_lst "Lstaticcatch"
-       [ hash_lambda l
-       ; hash_lambda r
+       [ hash_lambda' l
+       ; hash_lambda' r
        ]
   | Ltrywith (l,_,r) ->
      hash_string_lst "Ltrywith"
-       [ hash_lambda l
-       ; hash_lambda r
+       [ hash_lambda' l
+       ; hash_lambda' r
        ]
   | Lfor (_,a,b,d,c) ->
      hash_string_lst "Lfor"
-       [ hash_lambda a
-       ; hash_lambda b
+       [ hash_lambda' a
+       ; hash_lambda' b
        ; hash_direction d
-       ; hash_lambda c
+       ; hash_lambda' c
        ]
   | Lsend (m,a,b,xs,_) ->
      hash_string_lst "Lsend"
        [ hash_meth_kind m
-       ; hash_lambda a
-       ; hash_lambda b
-       ; hash_lst_anon hash_lambda xs
+       ; hash_lambda' a
+       ; hash_lambda' b
+       ; hash_lst_anon hash_lambda' xs
        ]
-  in hash_lambda
+  in hash_lambda []
 
 let sort_filter is_sorting threshold main_weight xs =
   let pred =
