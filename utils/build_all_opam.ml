a434ef8b1dcdb6ecd974998c49c72be7e03aa4a2
Alexandre Moine <alexandre@moine.me>
2019-09-28 18:47:37 +0200

    simple script to build lambdas for all opam

---
+++ utils/build_all_opam.ml
@@ -0,0 +1,54 @@
+let rec read_all_pkg desc =
+  try
+    let str = Unix.readdir desc in
+    if str = ".." || str = "."
+    then read_all_pkg desc
+    else str::read_all_pkg desc
+  with
+  | End_of_file -> []
+
+let test_problem should_fail x res =
+  if not (res = 0)
+  then
+    let str = "PROBLEM WITH: " ^ x in
+    if should_fail
+    then failwith str
+    else print_endline str; true
+  else false
+
+let install_pkg x =
+  let res = Sys.command ("opam install -y --switch=/home/nobrakal/prog/ocaml/ --show-actions " ^ x ^" | grep \"remove[ ]*ocaml-variants\"") in
+  if res = 0
+  then false
+  else
+    let res = Sys.command ("opam install -y --switch=/home/nobrakal/prog/ocaml/ " ^ x) in
+    if test_problem false x res
+    then false
+    else
+      let res = Sys.command ("PKG_NAME="^x^" opam reinstall -y --switch=/home/nobrakal/prog/ocaml/ " ^ x) in
+      not (test_problem true x res)
+
+let remove_head x xs =
+  match x with
+  | None -> xs
+  | Some x ->
+     let rec aux = function
+       | [] -> []
+       | y::ys ->
+          if x = y
+          then ys
+          else aux ys
+     in aux xs
+
+let main dir from =
+  let desc = Unix.opendir dir in
+  let pkg_list =
+    remove_head from @@
+      List.sort compare @@
+        read_all_pkg desc in
+  Unix.closedir desc;
+  let lst_ok = List.filter install_pkg pkg_list in
+  print_endline "INSTALLED PKGS";
+  List.iter print_endline lst_ok
+
+let () = main Sys.argv.(1) (try Some (Sys.argv.(2)) with | Invalid_argument _ -> None)
