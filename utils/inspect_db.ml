afa5680cc23e2767aa404c0ab9d6db78637db9d2
Alexandre Moine <alexandre@moine.me>
2019-10-10 09:04:14 +0200

    better inspect_db

--- utils/inspect_db.ml
+++ utils/inspect_db.ml
@@ -1,12 +1,5 @@
 open Asak.Wtree
 
-let rec to_list_append xs t =
-  match t with
-  | Leaf x -> x::xs
-  | Node (_,l,r) ->
-     let r = to_list_append xs r in
-     to_list_append r l
-
 let get_prefix x =
   try
     let ind = String.index x '.' in
@@ -26,44 +19,46 @@ let update_plus_one = function
   | None -> Some 1
   | Some x -> Some (x+1)
 
-let print_infos classes csvfile =
-  let list_of_lengths = List.rev_map List.length classes in
+let size_of_class = fold_tree (fun _ -> ( + ) ) List.length
+
+let print_infos (classes : string list wtree list) csvfile =
+  let list_of_lengths = List.rev_map size_of_class classes in
   let nb_defs = sum list_of_lengths in
   Printf.printf "Number of let-definitions: %d\n" nb_defs;
   let nb_classes = List.length classes in
   Printf.printf "Number of classes: %d\n" nb_classes;
   let real_class =
-    List.filter (function | [] | [_] -> false | _ -> true)
+    List.filter (function | Leaf _ -> false | _ -> true)
       classes in
   let nb_real_class = List.length real_class in
   Printf.printf "Number of classes with (strictly) more than one element: %d\n"
     nb_real_class;
-  let size_of_class = List.sort compare @@ List.map List.length real_class in
-  let maxe = last size_of_class in
+  let size_of_classes = List.sort compare @@ List.map size_of_class real_class in
+  let maxe = last size_of_classes in
   Printf.printf "The biggest class is of size: %d\n" maxe;
-  let median = List.nth (List.sort compare size_of_class) (nb_real_class / 2) in
+  let median = List.nth size_of_classes (nb_real_class / 2) in
   Printf.printf "Median of size of classes with more than one element: %d\n" median;
-  let assoc_map = List.fold_left (fun acc x -> IntMap.update x update_plus_one acc) IntMap.empty size_of_class in
+  let assoc_map = List.fold_left (fun acc x -> IntMap.update x update_plus_one acc) IntMap.empty size_of_classes in
   let plot = IntMap.fold (fun k v acc -> acc ^ string_of_int k ^ "," ^ string_of_int v ^ "\n") assoc_map "" in
   let chan = open_out csvfile in
   output_string chan plot;
   close_out chan
 
-let print_infos_without_versions before_clustering csvfile =
+let print_infos_without_versions (before_clustering : string list wtree list) csvfile =
   let class_with_only_lib_name =
     List.rev_map
-      (fun xs -> List.sort_uniq compare @@ List.rev_map get_prefix xs)
+      (fold_tree
+         (fun a b c -> Node (a,b,c))
+         (fun xs -> Leaf (List.sort_uniq compare @@ List.rev_map get_prefix xs)))
       before_clustering in
   print_infos class_with_only_lib_name csvfile
 
 let main filename csvfile =
   let chan = open_in_bin filename in
   let all_cluster : (string list) wtree list = Marshal.from_channel chan in
-  let before_clustering =
-    List.fold_left to_list_append [] all_cluster in
   print_endline "When considering different versions of the same package:";
-  print_infos before_clustering (csvfile ^ "all.csv");
+  print_infos all_cluster (csvfile ^ "all.csv");
   print_endline "When considering only one time a given definition for a given package:";
-  print_infos_without_versions before_clustering (csvfile ^ "uniq.csv")
+  print_infos_without_versions all_cluster (csvfile ^ "uniq.csv")
 
 let () = main Sys.argv.(1) Sys.argv.(2)
