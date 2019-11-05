dc9ec5675614448f553198fd9f2ce125c55abdcc
Gabriel Scherer <gabriel.scherer@gmail.com>
2019-11-05 21:14:08 +0100

    clustering_bench: inline wtree.ml for self-containedness

--- src/clustering_bench.ml
+++ src/clustering_bench.ml
@@ -5,10 +5,23 @@
  * asak is distributed under the terms of the MIT license. See the
  * included LICENSE file for details. *)
 
+module Wtree = struct
+  type 'a wtree =
+    | Node of (int * 'a wtree * 'a wtree)
+    | Leaf of 'a
+
+  let fold_tree n l =
+    let rec aux = function
+      | Leaf a -> l a
+      | Node (f,a,b) -> n f (aux a) (aux b)
+    in aux
+
+  let size_of_tree f =
+    fold_tree (fun _ a b -> 1 + a + b) f
+end
 open Wtree
 
 module Distance = struct
-
   type t = Regular of int | Infinity
 
   let compare x y =
