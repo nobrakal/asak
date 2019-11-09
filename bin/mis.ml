fd5af093f47cc3d84f736ba208d5dbc1bf3a467b
Alexandre Moine <alexandre@moine.me>
2019-11-09 11:20:10 +0100

    even better mis

--- bin/mis.ml
+++ bin/mis.ml
@@ -5,6 +5,13 @@ let load_file f =
   close_in ic;
   s
 
+let extract_dir_from_file f =
+  try
+    let ind = String.rindex f '/' in
+    String.sub f 0 ind, String.sub f (ind + 1) (String.length f - ind -1)
+  with
+  | Not_found -> ".",f
+
 let analysis database (name,hash) =
   match Asak.Clustering.HMap.find_opt hash database with
   | None -> ()
@@ -14,6 +21,7 @@ let analysis database (name,hash) =
      print_endline ""
 
 let get_typedtree load f =
+  Compmisc.init_path true;
   let cmt = Cmt_format.read_cmt f in
   match cmt.cmt_annots with
   | Implementation structure ->
@@ -25,34 +33,53 @@ let get_typedtree load f =
      cmt.cmt_modname,structure
   | _ -> failwith "not a structure"
 
+(** Get all lines starting by 'B' in a .merlin *)
 let build_from merlin =
   let full_file = load_file merlin in
   let lines = String.split_on_char '\n' full_file in
   let builds = List.filter (fun x -> String.length x > 0 && String.get x 0 = 'B') lines in
   List.map (fun x -> String.sub x 2 (String.length x - 2)) builds
 
-let prefix_of merlin =
-  try
-    let ind = String.rindex merlin '/' + 1 in
-    Some (String.sub merlin 0 ind)
-  with Not_found -> None
-
 let add_prefix prefix xs =
-  match prefix with
-  | None -> xs
-  | Some prefix ->
-     let aux x =
-       if String.get x 0 = '/'
-       then x
-       else prefix ^ x in
-     List.map aux xs
+  let aux x =
+    if String.get x 0 = '/'
+    then x
+    else prefix ^ x in
+  List.map aux xs
 
-let main database merlin typedtree =
-  print_endline "usage: mis.exe db.asak path/to/.merlin tree.cmt";
-  Compmisc.init_path true;
-  let database : string list Asak.Clustering.HMap.t =
-    Marshal.from_channel (open_in_bin database) in
-  let load = add_prefix (prefix_of merlin) (build_from merlin) in
+(** Given a file = "source.ml", will search for a file with a "Source.cmt" suffix
+    in the given path *)
+let find_cmt_in_paths file paths =
+  let mod_name =
+    let open String in
+    let start = sub file 0 (rindex file '.') in
+    let f = make 1 (Char.uppercase_ascii (get start 0)) in
+    f ^ (sub start 1 (length start - 1))
+  in
+  let cmt_end = mod_name ^ ".cmt" in
+  let is_suffix s x =
+    try
+      s = String.(sub x (length x - length s) (length s))
+    with
+    | Invalid_argument _ -> false in
+  let aux dir =
+    let files = Sys.readdir dir in
+    List.find (is_suffix cmt_end) (Array.to_list files) in
+  let res =
+    List.fold_left
+      (fun acc x ->
+        try Some (x ^ "/" ^ aux x)
+        with Not_found -> acc
+      ) None paths in
+  match res with
+  | None -> failwith "could not find the corresponding cmt"
+  | Some x -> x
+
+let main database full_file =
+  let dir,file = extract_dir_from_file full_file in
+  let merlin = dir ^ "/.merlin" in
+  let load =  add_prefix (dir ^ "/") (build_from merlin) in
+  let typedtree = find_cmt_in_paths file load in
   let name,typedtree = get_typedtree load typedtree in
   let lambdas = Asak.Parse_structure.read_structure name typedtree in
   let lambdas =
@@ -62,6 +89,11 @@ let main database merlin typedtree =
   let hash_list =
     Asak.Lambda_hash.(hash_all {should_sort=false; hash_var=true} 0 lambdas) in
   let main_hash_list = List.map (fun (x,(h,_)) -> x,h) hash_list in
+  let database : string list Asak.Clustering.HMap.t =
+    Marshal.from_channel (open_in_bin database) in
   List.iter (analysis database) main_hash_list
 
-let () = main Sys.argv.(1) Sys.argv.(2) Sys.argv.(3)
+let () =
+  print_endline "usage: mis.exe db.asak dir/file.ml";
+  print_endline "NB: dir must contains a .merlin";
+  main Sys.argv.(1) Sys.argv.(2)
