077a4eb3e7cce54fd1ff3e8026ebf1a3f18a4228
Alexandre Moine <alexandre@moine.me>
2023-10-26 13:11:28 +0200

    typo

--- src/partition.mli
+++ src/partition.mli
@@ -45,5 +45,5 @@ val create :
 val find_value_type_from_file : string -> string -> Types.type_expr
 
 (** Get the type of a value from a signature (usually obtained from a cmi).
-    Usage: [find_value_type_from_signature name file]. *)
+    Usage: [find_value_type_from_signature name signature]. *)
 val find_value_type_from_signature : string -> Types.signature_item list -> Types.type_expr
